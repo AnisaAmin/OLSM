2560ee7985060939bc195c1bd6a8dfb706aab99a
unknown <saudjkstar@gmail.com>
2019-01-27 12:08:11 +0530

    angry bird

--- frontEnd/src/app/app-routing.module.ts
+++ frontEnd/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from 'src/app/layout/main/main.component';
+import { SignInComponent } from 'src/app/sign-in/sign-in.component';
 
 const routes: Routes = [
-  { path : '' , component : MainComponent}
+  { path : '' , component : MainComponent},
+  { path : 'sign-in' , component : SignInComponent}
 ];
 
 @NgModule({
